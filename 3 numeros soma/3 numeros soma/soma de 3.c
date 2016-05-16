bb7c10b2ec5c08a96cb9901df3dd4b58af572053
jonipin <jonipin@hotmail.com>
2016-05-16 13:49:29 +0100

    Add files via upload

---
+++ 3 numeros soma/3 numeros soma/soma de 3.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>
+main() {
+	int a,b,c,res;
+	scanf("%d" ,&a);
+	scanf("%d" ,&b);
+	scanf("%d" ,&c);
+	res=a+b+c;
+	printf("A soma dos 3 numeros e : %d \n" ,res);
+	system("pause");
+}
